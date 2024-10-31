c73f703af7e2757dac0714e2d6d80980925acd0e
Young-Hyun Kwon <ehe151@naver.com>
2024-10-31 23:04:38 +0900

    Fix : 마지막 수정
    Fix : 마지막 수정
    
    - 안 쓰는 naemspace 정리
    - latency 디폴트 값 : 100으로 수정

--- Assets/Src/Codes/NetworkManager.cs
+++ Assets/Src/Codes/NetworkManager.cs
@@ -4,10 +4,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
 
